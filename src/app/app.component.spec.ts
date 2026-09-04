7217b207d9e6a67e65097780f67d2bdffe4215d7
Tomasz Janiszewski <tomash@tomash.pl>
2026-09-04 14:54:30 +0200

    (test): replace RouterTestingModule with provideRouter

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule, AppComponent]
+    imports: [AppComponent],
+    providers: [provideRouter([])]
   }));
 
   it('should create the app', () => {
