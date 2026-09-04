6e79ee330d918b19bc3073da554e3be5f326c959
Tomasz Janiszewski <tomash@tomash.pl>
2026-09-05 00:43:54 +0200

    (chore): scaffold the gallery ui workspace library

---
+++ projects/ui/src/lib/ui.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'ui-ui',
+  styles: ``,
+  template: `
+    <p>
+      ui works!
+    </p>
+  `,
+})
+export class Ui {
+}
